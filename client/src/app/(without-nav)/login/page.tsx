ef9ed87bd62b15dfff2b350f2d8f2ca8677469ad
Rava Maulana <ravamaulana14@gmail.com>
2023-05-14 10:17:36 +0700

    feat: added login page

--- client/src/app/(without-nav)/login/page.tsx
+++ client/src/app/(without-nav)/login/page.tsx
@@ -11,12 +11,11 @@ import ArrowRight from "@/assets/icons/arrow-right.svg";
 export default function SignupPage(): JSX.Element {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPass, setConfirmPass] = useState("");
 
   return (
     <main className="h-[100vh] w-full flex flex-col items-center justify-center">
       <h1 className="w-[64.1vw] tracking-[0.2px] font-jeko text-[32px] text-BROWN-600">
-        Register
+        Login
       </h1>
 
       <div className="mt-4">
@@ -37,16 +36,13 @@ export default function SignupPage(): JSX.Element {
         />
       </div>
 
-      <div className="mt-4">
-        <InputField
-          onChange={(e) => setConfirmPass(e.target.value)}
-          placeholder="repeat password"
-          value={confirmPass}
-          isPassword={true}
-        />
-      </div>
+      <div className="w-[64.1vw] flex flex-col items-end mt-5">
+        <Link href="/">
+          <p className="font-poppinsLight tracking-[0.2px] text-[10px] text-[#959595] mb-12 text-center w-[102px]">
+            Lupa password?
+          </p>
+        </Link>
 
-      <div className="w-[64.1vw] flex justify-end mt-14">
         <Link href="/dashboard">
           <button className="bg-BROWN-900 w-[102px] rounded-[10px] flex items-center justify-between pl-3.5 pr-2.5 py-2">
             <p className="font-jeko text-white text-xs tracking-[0.2px] pt-0.5">
